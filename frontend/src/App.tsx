69d940cb95addd5caa5f688eb43076c04231e977
dongcheulkim <dongcheul97@gmail.com>
2026-03-09 00:23:36 +0900

    자동 커밋: frontend/src/App.tsx,

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 
 import Used from './pages/Used';
+import UsedDetail from './pages/UsedDetail';
+import UsedRegister from './pages/UsedRegister';
 import Rental from './pages/Rental';
 import Lesson from './pages/Lesson';
 import Accommodation from './pages/Accommodation';
@@ -15,7 +17,9 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
-<Route path="used" element={<Used />} />
+          <Route path="used" element={<Used />} />
+          <Route path="used/register" element={<UsedRegister />} />
+          <Route path="used/:id" element={<UsedDetail />} />
           <Route path="rental" element={<Rental />} />
           <Route path="lesson" element={<Lesson />} />
           <Route path="accommodation" element={<Accommodation />} />
