96d198456bee2cf9f1615a0e9837b2a6c9e77f83
dongcheulkim <dongcheul97@gmail.com>
2026-03-09 00:18:26 +0900

    자동 커밋: frontend/src/App.tsx,

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
-import NewEquipment from './pages/NewEquipment';
+
 import Used from './pages/Used';
 import Rental from './pages/Rental';
 import Lesson from './pages/Lesson';
@@ -14,8 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
-          <Route path="new-equipment" element={<NewEquipment />} />
-          <Route path="used" element={<Used />} />
+<Route path="used" element={<Used />} />
           <Route path="rental" element={<Rental />} />
           <Route path="lesson" element={<Lesson />} />
           <Route path="login" element={<Login />} />
