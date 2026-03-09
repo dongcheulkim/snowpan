4e19588119f7ac8951ba7c32e9d80ab512acc562
dongcheulkim <dongcheul97@gmail.com>
2026-03-09 11:55:39 +0900

    자동 커밋: frontend/src/App.tsx,

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,7 @@ import Accommodation from './pages/Accommodation';
 import AccommodationDetail from './pages/AccommodationDetail';
 import Chat from './pages/Chat';
 import Community from './pages/Community';
+import CommunityDetail from './pages/CommunityDetail';
 import MyPage from './pages/MyPage';
 import SellerProfile from './pages/SellerProfile';
 import Login from './pages/Login';
@@ -35,6 +36,7 @@ function App() {
           <Route path="accommodation" element={<Accommodation />} />
           <Route path="accommodation/:id" element={<AccommodationDetail />} />
           <Route path="community" element={<Community />} />
+          <Route path="community/:id" element={<CommunityDetail />} />
           <Route path="mypage" element={<MyPage />} />
           <Route path="seller/:sellerId" element={<SellerProfile />} />
           <Route path="login" element={<Login />} />
